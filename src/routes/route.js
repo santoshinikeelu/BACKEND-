215857667172aafe285ddeeccb0ad74330ee1b7b
santoshinikeelu <dsantoshinipatra683@gmail.com>
2022-10-17 22:27:56 +0530

    get-api assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -40,4 +40,74 @@ router.get('/student-details/:name', function(req, res){
     res.send('Dummy response')
 })
 
+
+router .get('/movies', function (req, res){
+    var moviesname=["Rang de basanti","the shining" , "Lord of the rings", "Batman ","fidda","entertainment"]             //problem 1
+res.send(moviesname)
+})
+
+router.get('/movies/:indexnumber', function (req, res){
+    const arr=["Rang de basanti","the shining" , "Lord of the rings", "Batman ","fidda","entertainment"]          //problem 2
+    let index=req.params.indexnumber
+    res.send(arr[index])
+    
+})
+
+
+router.get('/movie/:index',function(req,res){
+    const arr=["Rang de basanti","the shining" , "Lord of the rings", "Batman ","fidda","entertainment"]
+    let index = req.params.index
+    if(index>arr.length){
+        res.send('Please use a valid index')
+    }
+    else{
+        res.send(arr[index])  
+    }                                                                                                                       //problem 3
+})
+
+router.get('/films',function(req,res){
+    const film=[ {
+        'id': 1,
+        'name': 'The Shining'
+       }, {                                                                                                       //problem 4
+        'id': 2,
+        'name': 'fidda'
+       }, {
+        'id': 3,
+        'name': 'Rang de basanti'
+       }, {
+        'id': 4,
+        'name': 'Finding Nemo'
+       }]
+       res .send(film)
+       
+       
+})
+
+router.get('/films/:filmid',function(req,res){
+    const film=[ {
+        'id': 1,
+        'name': 'The Shining'
+       }, {
+        'id': 2,
+        'name': 'fidda'
+       }, {
+        'id': 3,
+        'name': 'Rang de basanti'                                                                            //problem 5
+       }, {
+        'id': 4,
+        'name': 'Finding Nemo'
+       }]
+       let filmid=req.params.filmid
+       if (filmid>film.length){
+        res.send('Please use a valid index')
+    }
+    else{
+        res.send(film[filmid-1])
+    }
+            
+       })
+
+
+
 module.exports = router;
