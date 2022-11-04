5aa0f5f81e041ab8bc6c534615f362a90701c21d
santoshinikeelu <dsantoshinipatra683@gmail.com>
2022-11-04 10:43:07 +0530

    my assignment

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,38 +1,41 @@
 const orderModel = require("../models/orderModel")
 const productModel = require("../models/productModel")
 const userModel = require("../models/userModel")
+const {ObjectId,model, isValidObjectId} =require("mongoose")
 
 const createorder= async function (req, res) {
-    let order = req.body
-    let userid=order.user_Id
-    let productid=order.product_Id
+    
+    let userid=req.body.user_Id
+    let productid=req.body.product_Id
     console.log(userid)
-    let checkuserid= await userModel.findById(userid)
-    console.log(checkuserid)
-    if(!checkuserid){
+    
+    if(!isValidObjectId(userid)){
         return res.send("Invalid user Id ")
     }
-    
-    let checkproductid= await productModel.findOne({_id:productid})
-    if(!checkproductid){
+
+    if(!isValidObjectId(productid)){
         return res.send("Invalid product Id")
     }
+        let checkproductid= await productModel.findById(productid)
     let Price = checkproductid.price
+    let order=req.body
     console.log(Price)
     
-    
     order.isFreeAppUser=isFreeAppUser
-    if(order.isFreeAppUser=="true"){
-        order.amount=0
+    if(order.isFreeAppUser==="true"){
+       let result= order.amount=0
+        console.log(result)
+        let orderCreated = await orderModel.create(order)
+        res.send({data: orderCreated})
+    }
+    else{
     
-    let orderCreated = await orderModel.create(order)
-    res.send({data: orderCreated})}
 
-  else (order.isFreeAppUser=="false")
+ 
+    let checkuserid= await userModel.findById(userid)
+let Balance = checkuserid.balance
+    
    
-  
-let Balance =checkuserid .balance
-    console.log(Balance)
     if(Price>=Balance){
         return res.send("Insufficient Balance")
     }
@@ -44,10 +47,10 @@ let Balance =checkuserid .balance
    
         order.amount=Price
        
-    
     let orderCr = await orderModel.create(order)
     res.send({msg:orderCr,data:user1})
     console.log(orderCr)
-    }
+}
     
+} 
 module.exports.createorder = createorder
